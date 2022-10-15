cc6a56bbd12aff82522d8c2894adb58bd90f5870
DavidKozdra <davidkozdra@gmail.com>
2022-10-15 16:52:30 -0400

    Plater added third person camera worked on

--- Assets/PirateGame/Player/PlayerCamera.cs
+++ Assets/PirateGame/Player/PlayerCamera.cs
@@ -10,6 +10,8 @@ public class PlayerCamera : MonoBehaviour
     [Range(0,1)] public float TranslationPower = 0.9f;
     [Range(0,1)] public float RotationPower = 0.9f;
 
+    [Range(0,1)] public float OrbitPower = 0.9f;
+    [SerializeField] Vector3 OrbitDirection ;
     void LateUpdate()
     {
         Vector3 targetPosition = Target.position;
@@ -25,12 +27,15 @@ public class PlayerCamera : MonoBehaviour
 
         // flatten the direction to a plane defined by the up vector
         Vector3 flatDirection = Vector3.ProjectOnPlane(direction, up).normalized;
-
-        // 
+    
+        Vector3 toWorld = Target.TransformDirection(OrbitDirection);
+        flatDirection = Vector3.Slerp( flatDirection,  Vector3.ProjectOnPlane(toWorld,up).normalized,OrbitPower);
         Vector3 goalPosition = Target.position - flatDirection * Distance + up * Height;
         this.transform.position = Vector3.Lerp(this.transform.position, goalPosition, TranslationPower);
 
         Quaternion goalRotation = Quaternion.LookRotation(direction, up);
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, goalRotation, RotationPower);
+
+        
     }
 }
