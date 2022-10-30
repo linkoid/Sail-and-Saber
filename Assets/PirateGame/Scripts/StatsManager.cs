863c62e03a8e6970fd540deddd35d2836a6a9a09
DavidKozdra <davidkozdra@gmail.com>
2022-10-30 10:17:10 -0400

    Health Bar

--- Assets/PirateGame/Scripts/StatsManager.cs
+++ Assets/PirateGame/Scripts/StatsManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class StatsManager : MonoBehaviour
 {
+    public int Health;
+    public int MaxHealth;
+    public int Score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Health = MaxHealth;
     }
 
     // Update is called once per frame
