ee685391283b0b8c0b48984401c52b4366da6d15
DavidKozdra <davidkozdra@gmail.com>
2022-11-12 18:24:30 -0500

    Main menu
    scene transfer basics

---
+++ Assets/PirateGame/Environment/Scene_Transfer.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Scene_Transfer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    public void TargetScene(string SceneToTransfer){
+        if(SceneInBuild(SceneToTransfer)){
+            
+            Debug.Log(SceneToTransfer);
+            SceneManager.LoadScene(SceneToTransfer);
+        }else{
+            
+            Debug.Log(SceneToTransfer + " Does not exist in build index");
+        }
+        return; 
+    }
+
+    public void NextScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    //TODO binary search
+    public bool SceneInBuild(string  name){
+        Debug.Log(SceneManager.GetSceneByBuildIndex(0).name + " "  + SceneManager.sceneCountInBuildSettings);
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++){
+                if( SceneManager.GetSceneByBuildIndex(i).name == name){
+                    return true;
+                }else {
+                    Debug.Log( SceneManager.GetSceneByBuildIndex(i).name);
+                }
+        }
+        return false;
+    }
+}
