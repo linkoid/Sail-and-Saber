37b1700afa9df508fcdd6a15b371b6d0fcf90342
DavidKozdra <davidkozdra@gmail.com>
2022-11-19 19:40:52 -0500

    Main_menu Options

--- Assets/options.cs
+++ Assets/options.cs
@@ -6,9 +6,19 @@ using UnityEngine.InputSystem;
 
 public class options : MonoBehaviour
 {
+    public Slider VolumeSlider;
+    public Music musicPlayer;
+
     [SerializeField] bool isPaused =false;
     [SerializeField] GameObject Target;
+    private void Update()
+    {
+        musicPlayer.musicSource.volume = VolumeSlider.value;
+    }
     void OnPause(InputValue input){
+        if(!Target){
+            return;
+        }
         if(input.isPressed){
             isPaused = !isPaused;
             Time.timeScale = (isPaused) ? 0 : 1;
